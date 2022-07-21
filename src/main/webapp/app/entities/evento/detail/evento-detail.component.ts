834c704e966714dcc3ad1c731a9991c3e8dcb912
HernandezKev11 <Kevinhv28@hotmail.com>
2022-07-20 22:29:07 -0600

    Feat: añade correctamente la funcionalidad de cancelar eventos y luego poder eliminarlos

--- src/main/webapp/app/entities/evento/detail/evento-detail.component.ts
+++ src/main/webapp/app/entities/evento/detail/evento-detail.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IEvento } from '../evento.model';
-import { EventoDeleteDialogComponent } from '../cancel/evento-delete-dialog.component';
+import { EventoCancelDialogComponent } from '../cancel/evento-cancel-dialog.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { EventoDeleteDialogComponent } from '../delete/evento-delete-dialog.component';
 
 @Component({
   selector: 'jhi-evento-detail',
@@ -12,6 +13,7 @@ export class EventoDetailComponent implements OnInit {
   evento: IEvento | null = null;
   isPendiente = true;
   isEnProgreso = true;
+  isCancelado = true;
 
   constructor(protected activatedRoute: ActivatedRoute, protected modalService: NgbModal) {}
 
@@ -22,12 +24,19 @@ export class EventoDetailComponent implements OnInit {
         this.isPendiente = false;
       } else if (this.evento?.estado === 'En progreso') {
         this.isEnProgreso = false;
+      } else if (this.evento?.estado === 'Cancelado') {
+        this.isCancelado = false;
       }
     });
   }
 
   previousState(): void {
     window.history.back();
+  }
+
+  cancel(evento: IEvento): void {
+    const modalRef = this.modalService.open(EventoCancelDialogComponent, { size: 'lg', backdrop: 'static' });
+    modalRef.componentInstance.evento = evento;
   }
 
   delete(evento: IEvento): void {
--- src/main/webapp/app/entities/evento/evento.module.ts
+++ src/main/webapp/app/entities/evento/evento.module.ts
@@ -3,14 +3,22 @@ import { SharedModule } from 'app/shared/shared.module';
 import { EventoComponent } from './list/evento.component';
 import { EventoDetailComponent } from './detail/evento-detail.component';
 import { EventoUpdateComponent } from './create/evento-update.component';
-import { EventoDeleteDialogComponent } from './cancel/evento-delete-dialog.component';
+import { EventoCancelDialogComponent } from './cancel/evento-cancel-dialog.component';
 import { EventoRoutingModule } from './route/evento-routing.module';
 import { ResolverComponent } from './resolver/resolver.component';
+import { EventoDeleteDialogComponent } from './delete/evento-delete-dialog.component';
 
 @NgModule({
   imports: [SharedModule, EventoRoutingModule],
-  declarations: [EventoComponent, EventoDetailComponent, EventoUpdateComponent, EventoDeleteDialogComponent, ResolverComponent],
-  entryComponents: [EventoDeleteDialogComponent],
+  declarations: [
+    EventoComponent,
+    EventoDetailComponent,
+    EventoUpdateComponent,
+    EventoCancelDialogComponent,
+    ResolverComponent,
+    EventoDeleteDialogComponent,
+  ],
+  entryComponents: [EventoCancelDialogComponent],
   exports: [EventoComponent],
 })
 export class EventoModule {}
